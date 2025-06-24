db8a4d6d532c97da2eb1f387efb09c6e35db2cbf
oussamadzboud <dz6031629@gmail.com>
2025-06-24 13:11:09 +0100

    first commit

--- test/model/produitTest.java
+++ test/model/produitTest.java
@@ -1,59 +1,17 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+package model;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author admin
- */
 public class produitTest {
-    
-    public produitTest() {
+  @Test
+    public void testPrixTotal() {
+     
+        System.out.println("Test de la méthode prix_total");
+   produit p = new produit("", 500, 4);
+ int expResult = 2000;
+int result = p.prix_total(500, 4);
+assertEquals(expResult, result);
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of prix_total method, of class produit.
-     */
-    @Test
-    public void testPrix_total() {
-        System.out.println("prix_total");
-        int prix = 0;
-        int quantite = 0;
-        produit instance = null;
-        int expResult = 0;
-        int result = instance.prix_total(prix, quantite);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
-
-
